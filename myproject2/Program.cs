5207b7aa5b55dbcf608f34785abfd01eb1a85902
Drizbize <62217928+Drizbize@users.noreply.github.com>
2020-07-14 11:15:04 +0300

    Добавьте файлы проекта.

---
+++ myproject2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myproject2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = int.Parse(Console.ReadLine());
+            i = i % 2;
+            if (i == 0)
+                Console.WriteLine("четное");
+            if (i == 1)
+                Console.WriteLine("нечётное");
+            if (i == -1)
+                Console.WriteLine("нечётное");
+            Console.ReadKey();      
+        }
+    }
+}
